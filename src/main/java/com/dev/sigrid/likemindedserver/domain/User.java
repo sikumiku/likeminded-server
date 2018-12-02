a0712c569ea05e5c5fcbffaccef3e80c49b7f998
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-02 13:31:20 +0200

    test with different id generation strategy

--- src/main/java/com/dev/sigrid/likemindedserver/domain/User.java
+++ src/main/java/com/dev/sigrid/likemindedserver/domain/User.java
@@ -26,7 +26,7 @@ import java.util.*;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @NotBlank
     private String username;
